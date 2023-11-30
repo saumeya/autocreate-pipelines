f44c02c941ba944b790bacd7816eab33e6435fc6
Saumeya Katyal <saumeyakatyal@gmail.com>
2023-11-30 09:40:37 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This script assumes that you are connected to an OpenShift cluster and have the Pipelines Operator installed
+
+kubectl apply -f .
+
+tkn pipeline start say-things --showlog
+
+tkn pipeline start say-things --showlog
+
+tkn pipeline start say-things-in-order --showlog
